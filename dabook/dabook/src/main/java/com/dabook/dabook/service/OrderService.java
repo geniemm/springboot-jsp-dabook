e0394e9066b20fa5a45cc430697c5e0e51f02829
minjichoi <wjdtjs3558@naver.com>
2024-01-18 16:29:35 +0900

    주문 css 수정

---
+++ dabook/dabook/src/main/java/com/dabook/dabook/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.dabook.dabook.service;
+
+import com.dabook.dabook.dto.UserDTO;
+import com.dabook.dabook.repository.OrderRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    public List<UserDTO> userInfo(String no){
+        return null;
+    }
+
+}
